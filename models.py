ba024dc8a766141fdfdf569f2b6d9a70c5cb4f34
smnk-mcb <semikinnikolay1@gmail.com>
2025-05-05 12:31:04 +0600

    feat: postgresql model

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    fio = Column(String, nullable=False)
